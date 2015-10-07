a94a8e5e24b8a0d83f993c08b5ffb4b2941cb1fa
sascanagl <carl.nagle@sas.com>
2015-10-07 20:04:12 +0100

    Remove from distribution
    This test is not useful for general-purpose consumption of users.

--- src/install/Project/runEmbeddedTest.bat
+++
@@ -1,6 +0,0 @@
-
-start "Debug Log - Start" "%SAFSDIR%\Project\SAFSTESTLOG_Startup.bat"
-
-"%SAFSDIR%\jre\bin\java.exe" -Dsafs.project.config="%SAFSDIR%\project\EmbeddedTest.ini" org.safs.EmbeddedDCHookDriver
-
-start "Debug Log - Stop" "%SAFSDIR%\Project\SAFSTESTLOGShutdown.bat"
