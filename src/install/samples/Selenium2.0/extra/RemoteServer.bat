a666fa5e8a061a64d07c62f4a399701724a2d46c
leiwang008 <lei.wang@sas.com>
2015-11-04 10:42:39 +0800

    Detect the latest selenium-server and use it.

--- src/install/samples/Selenium2.0/extra/RemoteServer.bat
+++ src/install/samples/Selenium2.0/extra/RemoteServer.bat
@@ -1,3 +1,12 @@
 TITLE Remote Server
-set SE2BIN=%SAFSDIR%\samples\Selenium2.0\extra
-"%SAFSDIR%\jre\bin\java.exe" -jar "%SAFSDIR%\lib\selenium-server-standalone-2.41.0.jar" -Dwebdriver.chrome.driver="%SE2BIN%\chromedriver.exe" -Dwebdriver.ie.driver="%SE2BIN%\IEDriverServer.exe" -timeout=20 -browserTimeout=60
+@echo off
+set SAFSLIB=%SAFSDIR%\lib
+
+pushd %SAFSLIB%
+setlocal enableDelayedExpansion
+set max=0
+for /f "tokens=1* delims=-.0" %%A in ('dir /b /a-d selenium-server-standalone*.jar') do if %%B gtr !max! set max=%%B
+set SELENIUM_SERVER_JAR_LOC=%SAFSDIR%\lib\selenium-%max%
+popd
+
+"%SAFSDIR%\jre\bin\java.exe" -Xms512m -Xmx1g -jar "%SELENIUM_SERVER_JAR_LOC%" -Dwebdriver.chrome.driver="%SAFSLIB%\chromedriver.exe" -Dwebdriver.ie.driver="%SAFSLIB%\IEDriverServer.exe" -timeout=20 -browserTimeout=60
