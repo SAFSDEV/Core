7a6c7f3f344d0009e0308f2f7d7ac973e3ea5cf4
leiwang008 <lei.wang@sas.com>
2015-11-05 08:59:20 +0800

    Modify a tiny error for variable CLASSPATH.

--- src/install/samples/Selenium2.0/extra/RemoteServer.bat
+++ src/install/samples/Selenium2.0/extra/RemoteServer.bat
@@ -4,6 +4,6 @@ for /f "tokens=1* delims=-.0" %%A in ('dir /b /a-d %SAFSDIR%\lib\selenium-server
 set SELENIUM_SERVER_JAR_LOC=%SAFSDIR%\lib\selenium-%max%
 set EXECUTE=%SAFSDIR%/jre/Java64/jre/bin/java
 set SE2BIN=%SAFSDIR%/samples/Selenium2.0/extra
-set CLASSPATH=%SAFSDIR%\lib\safsselenium.jar;%SAFSDIR%\lib\%SELENIUM_SERVER_JAR_LOC%
+set CLASSPATH=%SAFSDIR%\lib\safsselenium.jar;%SELENIUM_SERVER_JAR_LOC%
 
 "%EXECUTE%" -Xms512m -Xmx2g -cp %CLASSPATH% org.safs.selenium.util.SeleniumServerRunner -jar "%SELENIUM_SERVER_JAR_LOC%" -Dwebdriver.chrome.driver="%SE2BIN%/chromedriver.exe" -Dwebdriver.ie.driver="%SE2BIN%/IEDriverServer.exe" -timeout=20 -browserTimeout=60
