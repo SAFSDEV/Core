787a598de679c98ab9ac3b074b3e9b021ad59864
leiwang008 <lei.wang@sas.com>
2015-11-09 11:55:32 +0800

    Provide the full path for command dir.

--- src/org/safs/selenium/install/extra/RemoteServer.bat
+++ src/org/safs/selenium/install/extra/RemoteServer.bat
@@ -1,13 +1,9 @@
 TITLE Remote Server
 @echo off
 
-cd %SELENIUM_PLUS%\libs
 setlocal enableDelayedExpansion
 set max=0
-for /f "tokens=1* delims=-.0" %%A in ('dir /b /a-d selenium-server-standalone*.jar') do if %%B gtr !max! set max=%%B
+for /f "tokens=1* delims=-.0" %%A in ('dir /b /a-d %SELENIUM_PLUS%\libs\selenium-server-standalone*.jar') do if %%B gtr !max! set max=%%B
 set SELENIUM_SERVER_JAR_LOC=%SELENIUM_PLUS%\libs\selenium-%max%
-cd %SELENIUM_PLUS%\extra
-
 
 "%SELENIUM_PLUS%\Java\jre\bin\java.exe" -Xms512m -Xmx1g -jar "%SELENIUM_SERVER_JAR_LOC%" -Dwebdriver.chrome.driver="%SELENIUM_PLUS%\extra\chromedriver.exe" -Dwebdriver.ie.driver="%SELENIUM_PLUS%\extra\IEDriverServer.exe" -timeout=20 -browserTimeout=60
-
