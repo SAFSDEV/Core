909e08931cb759b5600273975b7053d8873a3cc1
sascanagl <carl.nagle@sas.com>
2016-01-21 17:03:05 +0000

    Selenium2.0 sample updates for the SAFS WIN Release.

--- src/install/samples/Selenium2.0/runSE2ProcessContainer.bat
+++ src/install/samples/Selenium2.0/runSE2ProcessContainer.bat
@@ -1,5 +1,14 @@
+;
+;REM Start the SafsDriver to run the test
+;
+setlocal enableDelayedExpansion
+set max=0
+for /f "tokens=1* delims=-.0" %%A in ('dir /b /a-d %SAFSDIR%\lib\selenium-server-standalone*.jar') do if %%B gtr !max! set max=%%B
+set SELENIUM_SERVER_JAR_LOC=%SAFSDIR%\lib\selenium-%max%
 
-set CLASSPATH=%STAFDIR%\bin\JSTAF.jar;%SAFSDIR%\lib\safsselenium.jar;%SAFSDIR%\lib\selenium-server-standalone-2.45.0.jar
-"%SAFSDIR%\jre\bin\java.exe" -Dsafs.project.config="se2processcontainer.ini" -cp "%CLASSPATH%" org.safs.selenium.spc.WDSPC
+set CLASSPATH=%STAFDIR%\bin\JSTAF.jar;%SAFSDIR%\lib\safsselenium.jar;%SELENIUM_SERVER_JAR_LOC%
+set EXECUTE=%SAFSDIR%/jre/bin/java.exe
+
+"%EXECUTE%" -Dsafs.project.config="se2processcontainer.ini" -cp "%CLASSPATH%" org.safs.selenium.spc.WDSPC
 
 
