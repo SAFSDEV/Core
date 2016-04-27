8cf57aeada8abde9949dc5a8be99d6cd204cb715
sascanagl <carl.nagle@sas.com>
2016-04-27 22:50:44 +0100

    REST Testing API in Development
    The core of a REST Testing API that can be surfaced in other tools like
    SAFS/SeleniumPlus and others.  The API is not functional yet, but
    getting defined and is subject to change as development proceeds.

---
+++ src/org/safs/rest/service/Request.java
@@ -0,0 +1,136 @@
+/** 
+ * Copyright (C) SAS Institute, All rights reserved.
+ * General Public License: http://www.opensource.org/licenses/gpl-license.php
+ */
+
+package org.safs.rest.service;
+
+/**
+ * @author canagl
+ */
+public class Request {
+
+	String _request_line;
+	
+	String _request_method;
+	String _request_uri;
+	String _request_http_version;
+	
+	String _request_header;
+	String _general_header;
+	String _entity_header;
+	
+	Object _message_body;
+
+	/**
+	 * @return the _request_line
+	 */
+	public String get_request_line() {
+		return _request_line;
+	}
+
+	/**
+	 * @param _request_line the _request_line to set
+	 */
+	public void set_request_line(String _request_line) {
+		this._request_line = _request_line;
+	}
+
+	/**
+	 * @return the _request_method
+	 */
+	public String get_request_method() {
+		return _request_method;
+	}
+
+	/**
+	 * @param _request_method the _request_method to set
+	 */
+	public void set_request_method(String _request_method) {
+		this._request_method = _request_method;
+	}
+
+	/**
+	 * @return the _request_uri
+	 */
+	public String get_request_uri() {
+		return _request_uri;
+	}
+
+	/**
+	 * @param _request_uri the _request_uri to set
+	 */
+	public void set_request_uri(String _request_uri) {
+		this._request_uri = _request_uri;
+	}
+
+	/**
+	 * @return the _request_http_version
+	 */
+	public String get_request_http_version() {
+		return _request_http_version;
+	}
+
+	/**
+	 * @param _request_http_version the _request_http_version to set
+	 */
+	public void set_request_http_version(String _request_http_version) {
+		this._request_http_version = _request_http_version;
+	}
+
+	/**
+	 * @return the _request_header
+	 */
+	public String get_request_header() {
+		return _request_header;
+	}
+
+	/**
+	 * @param _request_header the _request_header to set
+	 */
+	public void set_request_header(String _request_header) {
+		this._request_header = _request_header;
+	}
+
+	/**
+	 * @return the _general_header
+	 */
+	public String get_general_header() {
+		return _general_header;
+	}
+
+	/**
+	 * @param _general_header the _general_header to set
+	 */
+	public void set_general_header(String _general_header) {
+		this._general_header = _general_header;
+	}
+
+	/**
+	 * @return the _entity_header
+	 */
+	public String get_entity_header() {
+		return _entity_header;
+	}
+
+	/**
+	 * @param _entity_header the _entity_header to set
+	 */
+	public void set_entity_header(String _entity_header) {
+		this._entity_header = _entity_header;
+	}
+
+	/**
+	 * @return the _message_body
+	 */
+	public Object get_message_body() {
+		return _message_body;
+	}
+
+	/**
+	 * @param _message_body the _message_body to set
+	 */
+	public void set_message_body(Object _message_body) {
+		this._message_body = _message_body;
+	}
+}
---
+++ src/org/safs/rest/service/Timeouts.java
@@ -0,0 +1,42 @@
+/** 
+ * Copyright (C) SAS Institute, All rights reserved.
+ * General Public License: http://www.opensource.org/licenses/gpl-license.php
+ */
+
+package org.safs.rest.service;
+
+/**
+ * Maintains default timeouts used during REST Requests.<br>
+ * Currently, there is only 1 type of timeout specified.<br>
+ * Future updates may provide for different timeout settings for different parts of the request and response.
+ * @author canagl
+ */
+public class Timeouts {
+
+	static Timeouts _defaultTimeouts = new Timeouts();
+	
+	long _millisToTimeout = 50;
+
+	public static Timeouts getDefaultTimeouts(){
+		return _defaultTimeouts;
+	}
+	public static void setDefaultTimeouts(Timeouts timeouts)throws IllegalArgumentException{
+		if(timeouts == null) throw new IllegalArgumentException("Default Timeouts parameter cannot be null.");
+		_defaultTimeouts = timeouts;
+	}
+	
+	public Timeouts(){}
+	
+	public Timeouts(long millisToTimeout) throws IllegalArgumentException{
+		setMillisToTimeout(millisToTimeout);
+	}
+	
+	public void setMillisToTimeout(long millisToTimeout) throws IllegalArgumentException{
+		if(millisToTimeout < 0) throw new IllegalArgumentException("Timeouts cannot be set less than 0.");
+		_millisToTimeout = millisToTimeout;
+	}
+	
+	public long getMillisToTimeout(){
+		return _millisToTimeout;
+	}
+}
