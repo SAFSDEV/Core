2af9864112bb54eae1d8934c2af5a936a39781de
sascanagl <carl.nagle@sas.com>
2016-06-17 19:09:08 +0100

    Significant updates primarily to JavaDoc only.

--- src/org/safs/logging/slf4j/CombinedLoggerAdapter.java
+++ src/org/safs/logging/slf4j/CombinedLoggerAdapter.java
@@ -6,23 +6,26 @@
 package org.safs.logging.slf4j;
 
 import org.safs.IndependantLog;
-import org.safs.logging.AbstractLogFacility;
 import org.slf4j.helpers.MessageFormatter;
 
 /**
+ * SAFS Logger Adapter writing to BOTH the SAFS Test Log and the SAFS Debug Log--if available.
+ * <p>
+ * This Logger will be acquired if requesting anything other than a "TEST" or "DEBUG" logname.
+ * <p>
+ * This Logger DOES require post-acquisition initialization.
+ * <p>
+ * Logger info, warn, and error messages will go to the SAFS Test Log.<br>
+ * Logger debug and trace messages will go to the SAFS Debug Log--if available.
+ * <p>
  * @author canagl
+ * @see SAFSLoggerFactory#getLogger(String)
+ * @see SAFSLoggerFactory#initializeTestLoggerAdapter(org.slf4j.Logger, org.safs.tools.drivers.AbstractDriver)
  */
 public class CombinedLoggerAdapter extends TestLoggerAdapter {
 
 	static final IndependantLog dlog = new IndependantLog();
 	
-	/**
-	 * 
-	 */
-	public CombinedLoggerAdapter() {
-		super();
-	}
-
 	@Override
 	public void debug(String msg) {
 		dlog.debug(msg);		
