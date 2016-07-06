e4d2c9e17a5f48fc42c7d07e3f178babddc47b56
sbjlwa <lei.wang@sas.com>
2016-07-06 15:29:56 +0800

    Compile groovy source code by FileSystemCompiler.

--- src/install/Project/runSAFS_CallJUnit_Groovy.bat
+++ src/install/Project/runSAFS_CallJUnit_Groovy.bat
@@ -6,26 +6,35 @@
 :: 
 :: [Prerequisite]
 :: 1. SAFS Installation.
-:: 2. Java/Groovy test files get compiled.
+:: 2. To compile Java/Groovy test files at runtime,
 ::    We need a java 32 bits compiler, which can be downloaded from 
 ::    http://download.oracle.com/otn/java/jdk/7u45-b18/jdk-7u45-windows-i586.exe
 ::    After installation, please set a environment %JAVA_HOME% pointing to the JDK
 ::    installation directory, for example JAVA_HOME=C:\JDK7_WIN
-::    We need a groovy compiler, which can be downloaded from http://groovy-lang.org/download.html
+::    For groovy compiler, we by default use the FileSystemCompiler to compile 
+::    groovy source code, it could work normally. If any problem, we could also 
+::    download a groovy compiler from http://groovy-lang.org/download.html
 ::    After installation, please set a environment %GROOVY_HOME% pointing to the groovy
 ::    compiler installation directory, for example GROOVY_HOME=C:\groovy-2.4.7
 ::
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+@REM Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" SETLOCAL
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Prepare environment variable for compilation and execution
 set max=0
 for /f "tokens=1* delims=-.0" %%A in ('dir /b /a-d %SAFSDIR%\lib\selenium-server-standalone*.jar') do if %%B gtr !max! set max=%%B
 set SELENIUM_SERVER_JAR_LOC=%SAFSDIR%\lib\selenium-%max%
-set COMPILE_CLASSPATH=%SELENIUM_SERVER_JAR_LOC%;%SAFSDIR%\lib\safs.jar;%CLASSPATH%
+set COMPILE_CLASSPATH=%SELENIUM_SERVER_JAR_LOC%;%SAFSDIR%\lib\safs.jar;%SAFSDIR%\lib\safsselenium.jar;%CLASSPATH%
 set OUTPUT_DIR=bin
 
-set GROOVYC=%GROOVY_HOME%\bin\groovyc.bat
+if "%GROOVY_HOME%"=="" (
+  set GROOVYC=org.codehaus.groovy.tools.FileSystemCompiler
+) else (
+  set GROOVYC=%GROOVY_HOME%\bin\groovyc.bat
+)
 set JAVAC=%JAVA_HOME%\bin\javac
 
 set EXECUTE=%SAFSDIR%\jre\bin\java.exe
@@ -39,7 +48,15 @@ set RUNTIME_CLASSPATH=%OUTPUT_DIR%;%COMPILE_CLASSPATH%
 ::    this project has been imported into Eclipse IDE and got compiled.
 rmdir /s /q %OUTPUT_DIR%
 mkdir %OUTPUT_DIR%
-call "%GROOVYC%" -cp "%COMPILE_CLASSPATH%" -d %OUTPUT_DIR% src/com/sas/spock/safs/runner/tests/SpockExperimentWithRunner.groovy
+:: 1.1 compile groovy source file. If we have groovyc installed, then we can use it to compile; 
+::     otherwise, we use org.codehaus.groovy.tools.FileSystemCompiler to compile.
+echo "Using %GROOVYC% to compile groovy source codes."
+if "%GROOVY_HOME%"=="" (
+  %EXECUTE% -cp "%COMPILE_CLASSPATH%" "%GROOVYC%" -d %OUTPUT_DIR% src/com/sas/spock/safs/runner/tests/SpockExperimentWithRunner.groovy
+) else (
+  call "%GROOVYC%" -cp "%COMPILE_CLASSPATH%" -d %OUTPUT_DIR% src/com/sas/spock/safs/runner/tests/SpockExperimentWithRunner.groovy
+)
+:: 1.2 compile java source file
 javac -cp "%COMPILE_CLASSPATH%" -encoding UTF-8 -d %OUTPUT_DIR% -nowarn src/com/sas/spock/safs/runner/tests/*.java
 Echo "Project has been compiled." 
 
@@ -47,3 +64,6 @@ Echo "Project has been compiled."
 "%EXECUTE%" -cp "%RUNTIME_CLASSPATH%" -Dsafs.project.config="calljunit_groovy_test.ini" org.safs.tools.drivers.SAFSDRIVER
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+@REM End local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" SETLOCAL
