1800abc2cc8c50374b664a17b08a5aa0db4e7a5f
sbjlwa <lei.wang@sas.com>
2016-07-20 15:30:58 +0800

    Remove redundant files, and modified some scripts to make them clear and easier to maintain.

--- src/install/bin/XSLQuickReference_IOS.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-@CLS
-
-call SetSAFSEnvironment.bat
-
-msxsl %SAFSXML%\XSLQuickReference.xml %SAFSXSL%\XSLQuickReferenceHTML.XSL engine=IOS -o %SAFSDOC%\IOSQuickReference.htm
---
+++ src/install/bin/_XSLBuildEngineReference.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+@CLS
+
+REM This batch is supposed to call with a parameter 'engine', which could be one of TIDComponent IOS Android RobotJ Selenium TestComplete Autoit
+
+call SetSAFSEnvironment.bat
+
+setlocal
+set ENGINE_NAME=%1
+
+msxsl %SAFSXML%\XSLDriverCommands.xml     %SAFSXSL%\XSLCoreReferenceLists.XSL engine=%ENGINE_NAME% -o %SAFSDOC%\%ENGINE_NAME%DriverCommandsList.htm
+msxsl %SAFSXML%\XSLComponentFunctions.xml %SAFSXSL%\XSLCoreReferenceLists.XSL engine=%ENGINE_NAME% -o %SAFSDOC%\%ENGINE_NAME%ComponentFunctionsList.htm
+msxsl %SAFSXML%\XSLEngineCommands.xml     %SAFSXSL%\XSLCoreReferenceLists.XSL engine=%ENGINE_NAME% -o %SAFSDOC%\%ENGINE_NAME%EngineCommandsList.htm
+msxsl %SAFSXML%\XSLDriverCommands.xml     %SAFSXSL%\XSLCoreReferencePrep.XSL  engine=%ENGINE_NAME% -o %SAFSBIN%\%ENGINE_NAME%DriverCommandsPrep.bat
+msxsl %SAFSXML%\XSLComponentFunctions.xml %SAFSXSL%\XSLCoreReferencePrep.XSL  engine=%ENGINE_NAME% -o %SAFSBIN%\%ENGINE_NAME%ComponentFunctionsPrep.bat
+msxsl %SAFSXML%\XSLEngineCommands.xml     %SAFSXSL%\XSLCoreReferencePrep.XSL  engine=%ENGINE_NAME% -o %SAFSBIN%\%ENGINE_NAME%EngineCommandsPrep.bat
+
+call %ENGINE_NAME%DriverCommandsPrep.bat
+call %ENGINE_NAME%ComponentFunctionsPrep.bat
+call %ENGINE_NAME%EngineCommandsPrep.bat
+
+endlocal
