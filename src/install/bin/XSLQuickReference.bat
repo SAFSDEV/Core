a76bfd9c23b1eb5a7e32dc34b242a87a25fe0b56
sbjlwa <lei.wang@sas.com>
2016-07-26 16:52:53 +0800

    Echo more information to better show the build progress.

--- src/install/bin/XSLQuickReference.bat
+++ src/install/bin/XSLQuickReference.bat
@@ -3,4 +3,6 @@
 
 call SetSAFSEnvironment.bat
 
+echo Generating SAFS quick reference document...
+
 msxsl %SAFSXML%\XSLQuickReference.xml %SAFSXSL%\XSLQuickReferenceHTML.XSL -o %SAFSDOC%\SAFSQuickReference.htm
--- src/install/bin/_XSLQuickReference.bat
+++ src/install/bin/_XSLQuickReference.bat
@@ -5,6 +5,8 @@ REM This batch is supposed to call with a parameter 'engine', which could be one
 
 call SetSAFSEnvironment.bat
 
+echo Generating quick reference for engine %ENGINE_NAME% ...
+
 setlocal
 set ENGINE_NAME=%1
 msxsl %SAFSXML%\XSLQuickReference.xml %SAFSXSL%\XSLQuickReferenceHTML.XSL engine=%ENGINE_NAME% -o %SAFSDOC%\%ENGINE_NAME%QuickReference.htm
