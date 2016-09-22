650180f02378577ed37724cddaafdd3c9b5b3f22
Terence Xie <xietao26@gmail.com>
2016-09-22 11:26:31 +0800

    Modify hierarchy.

--- src/org/safs/autoit/lib/AutoItLibrary.java
+++ src/org/safs/autoit/lib/AutoItLibrary.java
@@ -1,5 +1,19 @@
+/**
+ * Copyright (C) SAS Institute, All rights reserved.
+ * General Public License: http://www.opensource.org/licenses/gpl-license.php
+ **/
+
+/**
+*
+* History:<br>
+*
+*  <br>   SEP 22, 2016    (Tao Xie) Initial release, build SAFS version Java library to connect AutoIt API. Currently extend from 3rd part JAR 'autoitx4java'
+*  								   to add more basic AutoIt features like "Ctrl + Click" for SAFS keywords. 
+*/
 package org.safs.autoit.lib;
 
-public class AutoItLibrary {
+import autoitx4java.AutoItX;
+
+public class AutoItLibrary extends AutoItX {
 
 }
