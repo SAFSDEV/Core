269e63e85f2e7518288d33eff96bbf90d5ba8459
Terence Xie <xietao26@gmail.com>
2016-09-22 11:22:25 +0800

    Modify the hierarchy in Eclipse.

--- src/org/safs/autoit/lib/AutoItLibrary.java
+++ src/org/safs/autoit/lib/AutoItLibrary.java
@@ -1,19 +1,5 @@
-/**
- * Copyright (C) SAS Institute, All rights reserved.
- * General Public License: http://www.opensource.org/licenses/gpl-license.php
- **/
-
-/**
-*
-* History:<br>
-*
-*  <br>   SEP 22, 2016    (Tao Xie) Initial release, build SAFS version Java library to connect AutoIt API. Currently extend from 3rd part JAR 'autoitx4java'
-*  								   to add more basic AutoIt features like "Ctrl + Click" for SAFS keywords. 
-*/
 package org.safs.autoit.lib;
 
-import autoitx4java.AutoItX;
-
-public class AutoItLibrary extends AutoItX {
+public class AutoItLibrary {
 
 }
