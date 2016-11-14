53fbd9113c7a99cae4e63424667e3f129b81b987
sbjlwa <lei.wang@sas.com>
2016-11-14 10:41:27 +0800

    Added method convertWindowPosition(String) back and marked it as deprecated with comments "this method will be removed in the future".

--- src/org/safs/StringUtils.java
+++ src/org/safs/StringUtils.java
@@ -63,6 +63,7 @@ import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.safs.ComponentFunction.Window;
 import org.safs.sockets.DebugListener;
 import org.safs.text.FileUtilities;
 import org.safs.tools.CaseInsensitiveFile;
@@ -653,6 +654,27 @@ public class StringUtils{
 		 }
 	 }
 	 return props;
+  }
+  
+  /** 
+   * Convert window's position-size-status string of the formats:
+   * <ul>
+   * <li>"x;y;width;height;status"
+   * <li>"x,y,width,height,status"
+   * <li>"x y width height status"
+   * <li>"Coords=x;y;width;height;Status=status"
+   * <li>"Coords=x,y,width,height,Status=status"
+   * <li>"Coords=x y width height Status=status"
+   * </ul> 
+   * into a org.safs.ComponentFunction.Window object.
+   * 
+   * @param   windowPosition String, window's position-size-status string
+   * @return  org.safs.ComponentFunction.Window if successful, null otherwise
+   * @deprecated Please use {@link ComponentFunction#ConvertWindowPosition(String)} instead. <b>This method will be removed in the future.</b>
+   **/
+  @Deprecated
+  public static Window convertWindowPosition(String windowPosition) {
+	  return ComponentFunction.ConvertWindowPosition(windowPosition);
   }
   
   /** 
