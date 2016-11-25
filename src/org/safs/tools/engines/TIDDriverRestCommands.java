658fd053da26d45f2929cbad1c1581e34b8b5d09
sbjlwa <lei.wang@sas.com>
2016-11-26 00:56:09 +0800

    Implemented RestDeleteResponse RestDeleteResponseStore DeleteResponseStore and RestStoreResponse.

--- src/org/safs/tools/engines/TIDDriverRestCommands.java
+++ src/org/safs/tools/engines/TIDDriverRestCommands.java
@@ -10,16 +10,21 @@
  */
 package org.safs.tools.engines;
 
-import java.util.Iterator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 import org.safs.DriverCommand;
 import org.safs.IndependantLog;
+import org.safs.SAFSException;
 import org.safs.StringUtils;
 import org.safs.TestRecordHelper;
-import org.safs.logging.AbstractLogFacility;
 import org.safs.logging.LogUtilities;
 import org.safs.model.commands.DDDriverRestCommands;
+import org.safs.rest.service.Response;
+import org.safs.text.FAILKEYS;
 import org.safs.text.FAILStrings;
+import org.safs.text.GENKEYS;
 import org.safs.tools.drivers.DriverConstant;
 import org.safs.tools.drivers.DriverInterface;
 
@@ -99,6 +104,8 @@ public class TIDDriverRestCommands extends GenericEngine{
 		return testRecordData.getStatusCode();
 	}
 	
+	private static final Map<String, String> variablePrefixToResponse = new HashMap<String, String>();
+	
 	/**
 	 * Internal Driver Command Processor.
 	 */
@@ -109,7 +116,8 @@ public class TIDDriverRestCommands extends GenericEngine{
 			super();
 		}
 		
-		protected void localProcess(){
+		protected void init() throws SAFSException{
+			super.init();
 			mapname = testRecordData.getAppMapName();
 		}
 		
@@ -131,10 +139,13 @@ public class TIDDriverRestCommands extends GenericEngine{
 			String debugmsg = StringUtils.debugmsg(false);
 			String message = command;
 			String description = command;
-			Iterator<?> iter = params.iterator();
 			IndependantLog.debug(debugmsg+"processing"+command+"with parameters "+params);
 			
-			String variablePrefix = (String) iter.next();
+			//TODO do we need to add "responseID" as the first parameter, if we want to delete it also from the internal map.
+			String responseID = null;
+//			responseID = iterator.next();
+			
+			String variablePrefix = iterator.next();
 			if(!StringUtils.isValid(variablePrefix)){
 				message = failedText.convert("bad_param", "Invalid parameter value for variablePrefix", "variablePrefix");
 				standardErrorMessage(testRecordData, message, testRecordData.getInputRecord());
@@ -143,37 +154,112 @@ public class TIDDriverRestCommands extends GenericEngine{
 			}
 			
 			try { 
-				//TODO implement the details
-				logMessage( message, description, PASSED_MESSAGE);
-				setTRDStatus(testRecordData, DriverConstant.STATUS_NO_SCRIPT_FAILURE);
-				return;
+				int status = DriverConstant.STATUS_NO_SCRIPT_FAILURE;
+				
+				//TODO do we need to delete the Response from the internal map???
+//				Response response = TIDComponent.deleteRestResponse(responseID);
+//				
+//				if(response==null){
+//					message = failedText.convert(FAILKEYS.NO_SUCCESS_1, command+" was not successful.", command);
+//					description = failedText.convert(FAILKEYS.ID_NOT_FOUND_1, "Object identified by "+responseID+" was not found.", responseID);
+//					status = DriverConstant.STATUS_GENERAL_SCRIPT_FAILURE;
+//				}else{
+//					if(Response.delete(this, variablePrefix)){
+//						message = passedText.convert(GENKEYS.SUCCESS_1, command+" successful.", command);
+//						description = passedText.convert(GENKEYS.PREFIX_VARS_DELETE_1, 
+//								"Variables prefixed with '"+variablePrefix+"' have been delete.", variablePrefix);
+//					}else{
+//						message = failedText.convert(FAILKEYS.NO_SUCCESS_1, command+" was not successful.", command);
+//						description = failedText.convert(FAILKEYS.COULD_NOT_DELETE_PREFIX_VARS_1, 
+//								"Could not delete one or more variable values prefixed with '"+variablePrefix+"'.", variablePrefix);
+//						status = DriverConstant.STATUS_GENERAL_SCRIPT_FAILURE;
+//					}
+//				}
+				
+				if(variablePrefixToResponse.containsKey(variablePrefix)){
+					if(Response.delete(this, variablePrefix)){
+						message = passedText.convert(GENKEYS.SUCCESS_1, command+" successful.", command);
+						description = passedText.convert(GENKEYS.PREFIX_VARS_DELETE_1, 
+								"Variables prefixed with '"+variablePrefix+"' have been deleted.", variablePrefix);
+						variablePrefixToResponse.remove(variablePrefix);
+					}else{
+						message = failedText.convert(FAILKEYS.NO_SUCCESS_1, command+" was not successful.", command);
+						description = failedText.convert(FAILKEYS.COULD_NOT_DELETE_PREFIX_VARS_1, 
+								"Could not delete one or more variable values prefixed with '"+variablePrefix+"'.", variablePrefix);
+						status = DriverConstant.STATUS_GENERAL_SCRIPT_FAILURE;
+					}
+				}else{
+					message = failedText.convert(FAILKEYS.NO_SUCCESS_1, command+" was not successful.", command);
+					description = failedText.convert(FAILKEYS.ID_NOT_FOUND_1, 
+							"Object identified by "+variablePrefix+" was not found.", variablePrefix);
+					status = DriverConstant.STATUS_GENERAL_SCRIPT_FAILURE;
+				}
+				
+
+				
+				if(status==DriverConstant.STATUS_NO_SCRIPT_FAILURE){
+					logMessage( message, description, PASSED_MESSAGE);					
+				}else{
+					logMessage( message, description, FAILED_MESSAGE);
+				}
+				
+				setTRDStatus(testRecordData, status);
+
 			}catch(Exception e){
 				String exceptionMsg = StringUtils.debugmsg(e);
 				message = FAILStrings.convert(FAILStrings.GENERIC_ERROR, 
 						"*** ERROR *** "+exceptionMsg, exceptionMsg);
 				standardErrorMessage(testRecordData, message, testRecordData.getInputRecord());
 				setTRDStatus(testRecordData, DriverConstant.STATUS_GENERAL_SCRIPT_FAILURE);
-				return;
 			}
 		}
+		
 		private void deleteResponseStore(){
 			String debugmsg = StringUtils.debugmsg(false);
 			String message = command;
 			String description = command;
 			IndependantLog.debug(debugmsg+"processing"+command+"with parameters "+params);
 			
-			try { 
-				//TODO implement the details
-				logMessage( message, description, PASSED_MESSAGE);
-				setTRDStatus(testRecordData, DriverConstant.STATUS_NO_SCRIPT_FAILURE);
-				return;
+			try {
+				boolean success = true;
+				Set<String> variablePrefixes = variablePrefixToResponse.keySet();
+				StringBuffer successVariablePrefix = new StringBuffer();
+				StringBuffer failVariablePrefix = new StringBuffer();
+				
+				for(String variablePrefix:variablePrefixes){
+					success = Response.delete(this, variablePrefix);
+					if(success){
+						successVariablePrefix.append(variablePrefix+" ");
+						variablePrefixToResponse.remove(variablePrefix);
+					}else{
+						failVariablePrefix.append(variablePrefix+" ");
+					}
+				}
+				
+				String param = null;
+				if(success){
+					param = successVariablePrefix.toString().trim();
+					message = passedText.convert(GENKEYS.SUCCESS_1, command+" successful.", command);
+					description = passedText.convert(GENKEYS.PREFIX_VARS_DELETE_1, 
+							"Variables prefixed with '"+param+"' have been delete.", param);
+					logMessage( message, description, PASSED_MESSAGE);
+					setTRDStatus(testRecordData, DriverConstant.STATUS_NO_SCRIPT_FAILURE);
+				}else{
+					param = failVariablePrefix.toString().trim();
+					message = failedText.convert(FAILKEYS.NO_SUCCESS_1, command+" was not successful.", command);
+					description = failedText.convert(FAILKEYS.COULD_NOT_DELETE_PREFIX_VARS_1, 
+							"Could not delete one or more variable values prefixed with '"+param+"'.", param);
+					logMessage( message, description, FAILED_MESSAGE);
+					setTRDStatus(testRecordData, DriverConstant.STATUS_GENERAL_SCRIPT_FAILURE);
+				}
+				
+				
 			}catch(Exception e){
 				String exceptionMsg = StringUtils.debugmsg(e);
 				message = FAILStrings.convert(FAILStrings.GENERIC_ERROR, 
 						"*** ERROR *** "+exceptionMsg, exceptionMsg);
 				standardErrorMessage(testRecordData, message, testRecordData.getInputRecord());
 				setTRDStatus(testRecordData, DriverConstant.STATUS_GENERAL_SCRIPT_FAILURE);
-				return;
 			}
 		}
 		private void storeResponse(){
@@ -182,39 +268,66 @@ public class TIDDriverRestCommands extends GenericEngine{
 				return;
 			}
 			String debugmsg = StringUtils.debugmsg(false);
+			boolean saveRequest = false;
 			String message = command;
 			String description = command;
-			Iterator<?> iter = params.iterator();
 			IndependantLog.debug(debugmsg+"processing"+command+"with parameters "+params);
 			
-			String responseID = (String) iter.next();
+			String responseID = iterator.next();
 			if(!StringUtils.isValid(responseID)){
-				message = failedText.convert("bad_param", "Invalid parameter value for responseID", "responseID");
+				message = failedText.convert(FAILKEYS.BAD_PARAM, "Invalid parameter value for responseID", "responseID");
 				standardErrorMessage(testRecordData, message, testRecordData.getInputRecord());
 				setTRDStatus(testRecordData, DriverConstant.STATUS_GENERAL_SCRIPT_FAILURE);
 				return;
 			}
 			
-			String variablePrefix = (String) iter.next();
+			String variablePrefix = iterator.next();
 			if(!StringUtils.isValid(variablePrefix)){
-				message = failedText.convert("bad_param", "Invalid parameter value for variablePrefix", "variablePrefix ");
+				message = failedText.convert(FAILKEYS.BAD_PARAM, "Invalid parameter value for variablePrefix", "variablePrefix ");
 				standardErrorMessage(testRecordData, message, testRecordData.getInputRecord());
 				setTRDStatus(testRecordData, DriverConstant.STATUS_GENERAL_SCRIPT_FAILURE);
 				return;
+			}
+			
+			if(iterator.hasNext()){
+				saveRequest = Boolean.parseBoolean(iterator.next());
 			}
 			
 			try { 
-				//TODO implement the details
-				logMessage( message, description, PASSED_MESSAGE);
-				setTRDStatus(testRecordData, DriverConstant.STATUS_NO_SCRIPT_FAILURE);
-				return;
+				Response response = TIDComponent.getRestResponse(responseID);
+				int status = DriverConstant.STATUS_NO_SCRIPT_FAILURE;
+				
+				if(response==null){
+					message = failedText.convert(FAILKEYS.NO_SUCCESS_1, command+" was not successful.", command);
+					description = failedText.convert(FAILKEYS.ID_NOT_FOUND_1, "Object identified by "+responseID+" was not found.", responseID);
+					status = DriverConstant.STATUS_GENERAL_SCRIPT_FAILURE;
+				}else{
+					if(response.save(this, variablePrefix, saveRequest)){
+						message = passedText.convert(GENKEYS.SUCCESS_1, command+" successful.", command);
+						description = passedText.convert(GENKEYS.ID_OBJECT_SAVED_TO_VARIABLE_PREFIX_2, 
+								"Object identified by '"+responseID+"' has been saved to variables prefixed with '"+variablePrefix+"'", responseID, variablePrefix);
+						variablePrefixToResponse.put(variablePrefix, responseID);
+					}else{
+						message = failedText.convert(FAILKEYS.NO_SUCCESS_1, command+" was not successful.", command);
+						description = failedText.convert(FAILKEYS.COULD_NOT_SET_PREFIX_VARS_1, 
+								"Could not delete one or more variable values prefixed with '"+variablePrefix+"'.", variablePrefix);
+						status = DriverConstant.STATUS_GENERAL_SCRIPT_FAILURE;
+					}
+				}
+				
+				if(status==DriverConstant.STATUS_NO_SCRIPT_FAILURE){
+					logMessage( message, description, PASSED_MESSAGE);					
+				}else{
+					logMessage( message, description, FAILED_MESSAGE);
+				}
+				
+				setTRDStatus(testRecordData, status);
 			}catch(Exception e){
 				String exceptionMsg = StringUtils.debugmsg(e);
 				message = FAILStrings.convert(FAILStrings.GENERIC_ERROR, 
 						"*** ERROR *** "+exceptionMsg, exceptionMsg);
 				standardErrorMessage(testRecordData, message, testRecordData.getInputRecord());
 				setTRDStatus(testRecordData, DriverConstant.STATUS_GENERAL_SCRIPT_FAILURE);
-				return;
 			}
 		}
 	}
