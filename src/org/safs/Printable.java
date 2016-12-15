f403dcfedd7a6bdde69768008e6bbe1267722be3
sbjlwa <lei.wang@sas.com>
2016-12-15 17:06:31 +0800

    Introduce threshold so that the data will not be printed out if it is too big.

--- src/org/safs/Printable.java
+++ src/org/safs/Printable.java
@@ -16,6 +16,39 @@ package org.safs;
  *
  */
 public interface Printable {
+	/**
+	 * @return int, The number of tabulation to print before the actual message.
+	 */
 	public int getTabulation();
+	/**
+	 * @param tabulation int, The number of tabulation to print before the actual message.
+	 */
 	public void setTabulation(int tabulation);
+
+	/**
+	 * @param threshold int, the size of the data too big to print.
+	 */
+	public void setThreshold(int threshold);
+	/**
+	 * @return int, the size of the data too big to print.
+	 */
+	public int getThreshold();
+
+	/**
+	 * Enable/disable the "threshold ability".<br/>
+	 * If enabled, then the data bigger than the 'threshold' will not be printed out.
+	 * Otherwise, it will be printed out.
+	 *
+	 * @param enabled boolean, enable or disable
+	 */
+	public void setThresholdEnabled(boolean thresholdEnabled);
+	/**
+	 * If enabled, then the data bigger than the 'threshold' will not be printed out.
+	 * Otherwise, it will be printed out.
+	 * @return boolean, If "threshold ability" is enabled.
+	 */
+	public boolean isThresholdEnabled();
+
+	/** "** THE DATA IS TOO BIG TO PRINT **" */
+	public static final String DATA_BIGGER_THAN_THRESHOLD = "** THE DATA IS TOO BIG TO PRINT **";
 }
