c631117bf37b183f2ab13a6103ade73b9cbb70ff
John W. Lewis <johnw.lewis@sas.com>
2017-02-08 20:56:55 -0500

    Support for getting oauth2 information from a file.

---
+++ src/org/safs/rest/service/DelegateSafsrestAdapter.groovy
@@ -0,0 +1,32 @@
+package org.safs.rest.service
+
+import org.apache.hc.client5.http.testframework.HttpClientPOJOAdapter
+import org.safs.rest.service.models.consumers.SafsrestAdapter
+
+import groovy.lang.Delegate
+
+/**
+ *
+ * Because this class is instantiated by SAFS Core internally, there is a
+ * complication.  SAFS Core requires the adapters be of type
+ * org.apache.hc.client5.http.testframework.HttpClientPOJOAdapter which is
+ * in the SAFS Core project (temporarily hopefully).
+ * However, the SafsrestAdapter is not of that type (it is of type
+ * org.apache.hc.core5.testing.framework.ClientPOJOAdapter which is in
+ * HttpCore5.  Though these two ClientPOJOAdapter types
+ * are in different packages, they have the same API.  So, this
+ * class acts as a delegate effectively changing the type.
+ *
+ * TODO:  This complication can be resolved by changing SAS Core so it uses
+ * ClientPOJOAdapter from Apache HTTPCore5 instead of HttpClientPOJOAdapter.
+ *
+ */
+class DelegateSafsrestAdapter extends HttpClientPOJOAdapter {
+
+	@Delegate
+	SafsrestAdapter adapter = new SafsrestAdapter()
+	
+	public SafsrestAdapter getAdapter() {
+		adapter
+	}
+}
