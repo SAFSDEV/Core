d9de3ba92ed677d0c88724ec2e18ae8146f3298e
sbjlwa <lei.wang@sas.com>
2017-03-20 10:52:23 +0800

    Removed the ending spaces.

--- src/org/safs/rest/service/RESTImpl.java
+++ src/org/safs/rest/service/RESTImpl.java
@@ -16,10 +16,10 @@ import org.apache.hc.core5.http.NameValuePair;
 import org.apache.hc.core5.http.ProtocolVersion;
 
 public class RESTImpl {
-	
+
 	public Response request(String serviceId, String requestMethod, String relativeURI, String headers,
 			Object body) throws Exception {
-		
+
 		Service service = Services.getService(serviceId);
 		ProtocolVersion protVersion = service.getProtocolVersionObject();
 		String defaultURI = service.getBaseURL();
@@ -35,12 +35,12 @@ public class RESTImpl {
 		else
 			safsRequest.set_uri(defaultURI + "/" + relativeURI);
 		safsRequest.set_protocol_version(protVersion.toString());
-		
-		Response safsResponse = new Response();		
+
+		Response safsResponse = new Response();
 		safsResponse.set_request(safsRequest);
-		
+
 		Map<String,String> headersMap = Headers.getHeadersMapFromMultiLineString(headers);
-		
+
 		//TODO: Use HttpServerTestingFramework constants when they are added
 		Map<String,Object> request = new HashMap<String,Object>();
 		request.put("method", requestMethod);
@@ -53,29 +53,29 @@ public class RESTImpl {
 		request.put(PASSWORD, service.getPassword());
 
 		headersMap.remove(Headers.CONTENT_TYPE);
-		
+
 		long defaultTimeout = Timeouts.getDefaultTimeouts().getMillisToTimeout();
 		request.put("timeout", defaultTimeout);
-		
+
 		HttpClientPOJOAdapter clientAdapter = (HttpClientPOJOAdapter) service.getClientAdapter();
 
 		Map<String,Object> response = clientAdapter.execute(defaultURI, request);
-		
+
 		int status = (int) response.get("status");
-		
+
 		safsResponse.set_status_code(status);
-		
+
 		@SuppressWarnings("unchecked")
 		Map<String,String> respHeaders = (Map<String,String>) response.get("headers");
-		
+
 		safsResponse.set_headers(respHeaders);
-		
+
 		safsResponse.set_entity_body(response.get("body"));
 		safsResponse.set_content_type((String) response.get("contentType"));
-		
+
 		return safsResponse;
 	}
-	
+
 	/**
 	 * Parse a URI and put the parameters in a query map.  Also, return the
 	 * path with the parameters taken off.
@@ -84,7 +84,7 @@ public class RESTImpl {
 		URI uri = new URI(uriString);
 		Map<String, String> query = new HashMap<String,String>();
 		List<NameValuePair> nvplist = URLEncodedUtils.parse(uri, StandardCharsets.UTF_8);
-		
+
 		for (NameValuePair nvp : nvplist) {
 			query.put(nvp.getName(), nvp.getValue());
 		}
@@ -93,5 +93,5 @@ public class RESTImpl {
 		ret.put("path", uri.getPath());
 		return ret;
 	}
-	
+
 }
