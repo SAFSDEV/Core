5279a05b2e8a214d9e6f83d88b56075c23130911
sbjlwa <lei.wang@sas.com>
2017-04-10 16:16:36 +0800

    Removed ending spaces.

--- src/org/safs/install/LibraryUpdate.java
+++ src/org/safs/install/LibraryUpdate.java
@@ -2,9 +2,9 @@
  ** General Public License: http://www.opensource.org/licenses/gpl-license.php
  **/
 package org.safs.install;
-/** 
+/**
  * History:
- * 
+ *
  *    JUL 03, 2015	(LeiWang)	Modify prepareTempSourceDirectory(): Move the download and unzip functionality out of this method.
  *                                                                   Call these functionalities in downloader thread directly, wait for unzip done before updateDiretory.
  *    JUL 15, 2015	(LeiWang)	Modify processArgs(): Catch Throwable to avoid infinite loop waiting.
@@ -34,7 +34,7 @@ import javax.swing.JOptionPane;
 import org.safs.text.FileUtilities;
 
 /**
- * A generic customizable File directory updater for SAFS users wishing to update certain library assets 
+ * A generic customizable File directory updater for SAFS users wishing to update certain library assets
  * with newer assets that might be available on the network in a central location.
  * <p>
  * For example, users can use this to grab centrally located build assets to update the SAFS\lib JAR files.
@@ -53,7 +53,7 @@ import org.safs.text.FileUtilities;
  * <p>
  * In addition, only source assets dated newer than Aug 01, 2012 are considered for update.
  * <p>
- * The class supports backing up older files into any specified directory.  
+ * The class supports backing up older files into any specified directory.
  * It also supports directory recursion, and an option to override the prompting for file replacement.
  * <p>
  * There are no SAFS dependencies in this class and can be packaged stand-alone in an executable JAR file.
@@ -62,10 +62,10 @@ import org.safs.text.FileUtilities;
  * <p><code><big><ul><table>
  * <tr><td style="vertical-align: top;white-space:nowrap;">-q           <td>(quiet mode, no prompt dialog), it should be put before other argument if present"
  * <tr><td style="vertical-align: top;white-space:nowrap;">"-prompt:titlebar" <td>(alternate prompt title bar message)<br>
- * This parameter should appear before other parameters (at this time) 
+ * This parameter should appear before other parameters (at this time)
  * <tr ><td style="vertical-align: top;white-space:nowrap;">-b:backupdir <td>**(backup dir for replaced files)
  * <tr><td style="vertical-align: top;white-space:nowrap;">-s:sourcedir <td>* (source dir containing newer files, or an HTTP URL to a ZIP)<br>
- * Supports HTTP URL to a ZIP file that would be downloaded and extracted into a temporary directory 
+ * Supports HTTP URL to a ZIP file that would be downloaded and extracted into a temporary directory
  * to be used as the sourcedir for the update.<br>
  * Note: Any spaces in the URL should be retained as spaces--not URLEncoded.<br>
  * The code will handle URL encoding of spaces.<br>
@@ -75,7 +75,7 @@ import org.safs.text.FileUtilities;
  * <tr><td style="vertical-align: top;white-space:nowrap;">-r           <td>(recurse into sub-directories)
  * <tr><td style="vertical-align: top;white-space:nowrap;">-a           <td>(file of all types will be copy, default false)"
  * <tr><td style="vertical-align: top;white-space:nowrap;">-nob         <td>(no file backups required)
- * <tr><td> &nbsp;&nbsp; <td> &nbsp;&nbsp;   
+ * <tr><td> &nbsp;&nbsp; <td> &nbsp;&nbsp;
  * <tr><td><td>*  - required parameter.
  * <tr><td><td>** - only required if -nob not provided.
  * </table></big></code></ul>
@@ -91,7 +91,7 @@ import org.safs.text.FileUtilities;
 public class LibraryUpdate {
 
 	public static final String _VERSION_ = "1.0";
-	
+
 	public static final String ARG_PREFIX_B = "-b:";
 	public static final String ARG_PREFIX_NOB = "-nob";
 	public static final String ARG_PREFIX_P = "-prompt:";
@@ -101,13 +101,13 @@ public class LibraryUpdate {
 	public static final String ARG_PREFIX_F = "-f";
 	public static final String ARG_PREFIX_A = "-a";
 	public static final String ARG_PREFIX_Q = "-q";
-    
+
     // do not attempt to copy/replace files older than this date
     public static final long OLDEST_DATE = Date.parse("01 Aug 2012");
-    
+
 	PrintStream console = System.out;
 	PrintStream errors = System.err;
-	
+
 	String prompt = "SAFS Library Update "+ _VERSION_;
 	boolean recurse = false;
 	boolean force = false;
@@ -118,48 +118,48 @@ public class LibraryUpdate {
 	/** Gets set TRUE if the user cancelled the update from a dialog prompt. */
 	public boolean canceled = false;
 	public String canceledMSG = null;
-	
+
 	File targetdir = null;
 	File sourcedir = null;
 	File backupdir = null;
 	int modifiedFiles = 0;
 	int backupFiles = 0;
-	
+
 	/**
 	 * If we need to download zip file from Internet and unzip it in a separate thread,<br>
 	 * we need to set this field to false in the main thread. In the separate thread, when<br>
 	 * unzip finishes, this field will be set to true. In the main thread, we can wait this<br>
 	 * field becomes true before processing some actions like {@link #updateDirectory(File, File, File)}.<br>
-	 * 
+	 *
 	 * @see #updateDirectory(File, File, File)
 	 */
 	boolean unzipDone = true;
 	/**
 	 * If we need to download file from Internet in a separate thread,<br>
 	 * we need to set this field to false in the main thread.<br>
-	 * 
+	 *
 	 * @see #processArgs(String[])
 	 */
 	boolean downloadDone = false;
-	final ProgressIndicator progressor = new ProgressIndicator();	
+	final ProgressIndicator progressor = new ProgressIndicator();
     int progress = 0;
-	
+
 	/**
 	 * Default constructor using standard System.out and System.err console streams.
 	 */
 	public LibraryUpdate(){
 		this(System.out, System.err);
 	}
-	
+
 	/**
 	 * Alternate constructor provide different console and error output streams.
 	 */
-	public LibraryUpdate(PrintStream console, PrintStream errors){ 
+	public LibraryUpdate(PrintStream console, PrintStream errors){
 		this.console = console;
 		this.errors = errors;
 		init();
 	}
-	
+
 	protected void init(){
 	    javax.swing.SwingUtilities.invokeLater(new Runnable() {
 	        public void run() {
@@ -167,13 +167,13 @@ public class LibraryUpdate {
 	        }
 	    });
 	}
-	
+
 	/**
 	 * Main entry point for an instance of the class not executing from the command-line.
 	 * @param args command-line args
 	 * @return true if the args processed OK and the updateDirectory process completed successfully.
-	 * returns false if the user canceled the update or an error occurred in processing the args or 
-	 * performing the update.  
+	 * returns false if the user canceled the update or an error occurred in processing the args or
+	 * performing the update.
 	 * @throws Exception
 	 */
 	public boolean processArgs(String[] args)throws Exception{
@@ -182,7 +182,7 @@ public class LibraryUpdate {
 		canceledMSG = null;
 		String message = null;
 		for(String arg: args){
-			
+
 			if(arg.startsWith(ARG_PREFIX_T)){
 				targetdir = new File(arg.substring(ARG_PREFIX_T.length()));
 				if(! targetdir.canWrite()) throw new Exception("Specified target directory '"+ targetdir.getAbsolutePath() +"' cannot be written.");
@@ -194,19 +194,19 @@ public class LibraryUpdate {
 				if(sarg.toLowerCase().startsWith("http")){
 
 					progressor.setProgressInfo(progress+=10, "Evaluating URL Source: "+ sarg);
-					
+
 					if(!quiet){
 						message = "Preparing to download a library update.\n\n"
 								+ "Source: "+ sarg +"\n"
 								+ "\nThis may take a few minutes.\n"
 								+ "\nDo you wish to proceed?\n";
 						Object[] options = {
-								"Proceed with Download", 
+								"Proceed with Download",
 								"Exit without Download"
 						};
-						int selected = JOptionPane.showOptionDialog(null, 
-								message, 
-								prompt, 
+						int selected = JOptionPane.showOptionDialog(null,
+								message,
+								prompt,
 								JOptionPane.YES_NO_OPTION,
 								JOptionPane.QUESTION_MESSAGE,
 								null,
@@ -218,7 +218,7 @@ public class LibraryUpdate {
 							return false;
 						}
 					}
-					
+
 					Thread downloader = new Thread(new Runnable(){
 						public void run(){
 							File zipfile = null;
@@ -279,13 +279,13 @@ public class LibraryUpdate {
 						return false;
 					}
 					progressor.setProgressInfo(progress, "Download complete. Evaluating content...");
-					
+
 					//Before updating files from sourcedir to targetdir, we DO need to wait
 					//unzip finished in the sourcedir.
 					//If some Exception happened during unzip, the field canceled will be set to true.
 					if(!unzipDone){
 						progressor.setProgressMessage("Waiting for files unzipping done ...");
-						while(!canceled && !unzipDone){ 
+						while(!canceled && !unzipDone){
 							try{Thread.sleep(1000);}catch(Exception x){}
 						}
 					}
@@ -353,10 +353,10 @@ public class LibraryUpdate {
 			}
 		}
 		progressor.setProgressInfo(progress+=10, "Preparing to perform the update...");
-		if ((targetdir != null && targetdir.canWrite()) && 
-		   (sourcedir != null && sourcedir.canRead())  && 
+		if ((targetdir != null && targetdir.canWrite()) &&
+		   (sourcedir != null && sourcedir.canRead())  &&
 		   (!dobackup || (backupdir != null && backupdir.canWrite()))){
-			
+
 			if(!quiet){
 				message = "Preparing to perform a library update.\n\n"
 						+ "Source: "+ sourcedir +"\n"
@@ -365,14 +365,14 @@ public class LibraryUpdate {
 						+ "Backup: "+ dobackup +"\n";
 				if(dobackup) message += "Backups: "+ backupdir +"\n";
 				message += "\nDo you wish to proceed?\n";
-				
+
 				Object[] options = {
-						"Proceed with Update", 
+						"Proceed with Update",
 						"Exit without Update"
 				};
-				int selected = JOptionPane.showOptionDialog(null, 
-						message, 
-						prompt, 
+				int selected = JOptionPane.showOptionDialog(null,
+						message,
+						prompt,
 						JOptionPane.YES_NO_OPTION,
 						JOptionPane.QUESTION_MESSAGE,
 						null,
@@ -384,7 +384,7 @@ public class LibraryUpdate {
 					return false;
 				}
 			}
-			
+
 			progressor.setProgressMessage("Updating...");
 			updateDirectory(targetdir, sourcedir, backupdir);
 			progressor.setProgressMessage("Update complete.");
@@ -393,8 +393,8 @@ public class LibraryUpdate {
 		return false;
 	}
 
-	public static final String help_string = "\n" 
-			   + "SAFS LibraryUpdate "+ _VERSION_ +"\n" 
+	public static final String help_string = "\n"
+			   + "SAFS LibraryUpdate "+ _VERSION_ +"\n"
 			   + "\n"
 			   + "Command-line arguments: \n"
 			   + "\n"
@@ -409,7 +409,7 @@ public class LibraryUpdate {
 			   + "   -a				   (file of all types will be copy, default false)\n\n"
 			   + "*  - required parameter.\n"
 			   + "** - only required if -nob not provided.\n\n";
-	
+
 	static void help(PrintStream out){
 		out.print(help_string);
 	}
@@ -419,7 +419,7 @@ public class LibraryUpdate {
 	static final String LC_DAT = ".dat";
 	static final String LC_EXE = ".exe";
 	static final String LC_TSCRIPT = ".tscript";
-	
+
 	/**
 	 * Include only .JAR, .SVB, and .DAT Files newer than OLDEST_DATE
 	 * @param file
@@ -428,13 +428,13 @@ public class LibraryUpdate {
 	boolean fileIncluded(File file){
 		if(file.getName().length()==0) return false;
 		String lcname = file.getName().trim().toLowerCase();
-		return ((lcname.endsWith(LC_JAR) || 
-		         lcname.endsWith(LC_SVB) ||	
-		         lcname.endsWith(LC_EXE) ||	
+		return ((lcname.endsWith(LC_JAR) ||
+		         lcname.endsWith(LC_SVB) ||
+		         lcname.endsWith(LC_EXE) ||
 		         lcname.endsWith(LC_DAT)) &&
 		        (file.lastModified() > OLDEST_DATE));
 	}
-	
+
 	/**
 	 * Include all Files newer than OLDEST_DATE
 	 * @param file
@@ -448,10 +448,10 @@ public class LibraryUpdate {
 	// storage in-case we have to back out changes
 	// key = targetfile, value = backupfile
 	HashMap backups = new HashMap();
-	
+
 	/**
 	 * Uses copyFile to backup a targetfile to a backup directory.
-	 * Retains a storage reference to every file backed up in the event 
+	 * Retains a storage reference to every file backed up in the event
 	 * that an attempt is made to roll-back the update following some type of update failure.
 	 * @param targetfile
 	 * @param backupdir
@@ -463,7 +463,7 @@ public class LibraryUpdate {
 		backups.put(targetfile, backupfile);
 		backupFiles++;
 	}
-	
+
 	/**
 	 * Copy a sourcefile to a targetfile location.
 	 * Will delete an existing file if it already exists before creating the replacement file.
@@ -487,9 +487,9 @@ public class LibraryUpdate {
 		}
 		targetfile.setLastModified(sourcefile.lastModified());
 	}
-	
+
 	/**
-	 * Re-entrant routine to process the provided directories.  If recurse is true, 
+	 * Re-entrant routine to process the provided directories.  If recurse is true,
 	 * the routine will call itself with each new source subdirectory encountered.
 	 * @param targetdir
 	 * @param sourcedir
@@ -508,7 +508,7 @@ public class LibraryUpdate {
 			"Yes to All",
 			"Cancel"
 		};
-		
+
 		SimpleDateFormat date = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
 		try{
 			for(File file:sourcefiles){
@@ -516,13 +516,13 @@ public class LibraryUpdate {
 					if(recurse) sourcesubs.add(file);
 					continue;
 				}
-				
+
 				if (allfilecopy){
 					if (! allFileIncluded(file)) continue;
 				} else {
 					if(! fileIncluded(file)) continue;
 				}
-				
+
 				targetfile = new File(targetdir, file.getName());
 				if(targetfile.exists()){
 					if(targetfile.lastModified() >= file.lastModified()) continue;
@@ -535,7 +535,7 @@ public class LibraryUpdate {
 					sourceinfo += "\nSize: "+ file.length();
 					if( file.length() > targetfile.length())
 						sourceinfo += " (larger)";
-					
+
 					targetinfo = "Target: "+ targetfile.getAbsolutePath()+ "\n"
 							   + "TimeStamp: "+ date.format(new Date(targetfile.lastModified()));
 					if (targetfile.lastModified() > file.lastModified())
@@ -543,7 +543,7 @@ public class LibraryUpdate {
 					targetinfo += "\nSize: "+ targetfile.length();
 					if( targetfile.length() > file.length())
 						targetinfo += " (larger)";
-					
+
 					promptMessage = "\n Preparing to ";
 					if(targetfile.exists() && dobackup)
 						promptMessage += "Backup and ";
@@ -552,10 +552,10 @@ public class LibraryUpdate {
 							       + "\n\n"
 							       + targetinfo
 							       + "\nDo you wish to proceed?\n\n";
-					
-					int selected = JOptionPane.showOptionDialog(null, 
-							promptMessage, 
-							prompt, 
+
+					int selected = JOptionPane.showOptionDialog(null,
+							promptMessage,
+							prompt,
 							JOptionPane.YES_NO_CANCEL_OPTION,
 							JOptionPane.QUESTION_MESSAGE,
 							null,
@@ -567,7 +567,7 @@ public class LibraryUpdate {
 					}
 					if(selected == 1) force = true;
 				}
-				if( targetfile.exists() && dobackup) backupFile(targetfile, backupdir);				
+				if( targetfile.exists() && dobackup) backupFile(targetfile, backupdir);
 				copyFile(file, targetfile);
 				modifiedFiles++;
 			}
@@ -607,7 +607,7 @@ public class LibraryUpdate {
 			throw x;
 		}
 	}
-	
+
 	/** Prepare temporary directory for holding files downloaded from Internet.*/
 	File prepareTempSourceDirectory() throws FileNotFoundException, IOException{
 		File tempfile = null;
@@ -617,12 +617,12 @@ public class LibraryUpdate {
 		tempfile.delete();
 		tempdir = new File(tempdir, "SAFS_UPDATE");
 		tempdir.mkdir();
-		
+
 		File[] files = tempdir.listFiles();
 		if(files != null && files.length > 0){
 			for(File file:files){
 				if(file.isDirectory()) {
-					try{ 
+					try{
 						FileUtilities.deleteDirectoryRecursively(file.getAbsolutePath(), false);
 					}catch(Throwable x){
 						// java.lang.NoClassDefFoundError
@@ -640,7 +640,7 @@ public class LibraryUpdate {
 		if(tempdir==null || !tempdir.isDirectory()) throw new IOException("Cannot deduce a root source directory for HTTP extraction.");
 		return tempdir;
 	}
-	
+
 	void downloadURL(String url, File outfile) throws MalformedURLException, IOException{
 		InputStream in = null;
 		BufferedOutputStream fout = null;
@@ -682,18 +682,18 @@ public class LibraryUpdate {
 	public boolean isQuiet(){
 		return quiet;
 	}
-	
+
 	/**
 	 * The main entry point for the application when invoked via the command-line.
-	 * @param args 
+	 * @param args
 	 * Command-line options:
 	 * <p><code><big><ul><table>
 	 * <tr><td style="vertical-align: top;white-space:nowrap;">-q           <td>(quiet mode, no prompt dialog), it should be put before other argument if present"
 	 * <tr><td style="vertical-align: top;white-space:nowrap;">"-prompt:titlebar" <td>(alternate prompt title bar message)<br>
-	 * This parameter should appear before other parameters (at this time) 
+	 * This parameter should appear before other parameters (at this time)
 	 * <tr ><td style="vertical-align: top;white-space:nowrap;">-b:backupdir <td>**(backup dir for replaced files)
 	 * <tr><td style="vertical-align: top;white-space:nowrap;">-s:sourcedir <td>* (source dir containing newer files, or an HTTP URL to a ZIP)<br>
-	 * Supports HTTP URL to a ZIP file that would be downloaded and extracted into a temporary directory 
+	 * Supports HTTP URL to a ZIP file that would be downloaded and extracted into a temporary directory
 	 * to be used as the sourcedir for the update.<br>
 	 * Note: Any spaces in the URL should be retained as spaces--not URLEncoded.<br>
 	 * The code will handle URL encoding of spaces.<br>
@@ -703,16 +703,16 @@ public class LibraryUpdate {
 	 * <tr><td style="vertical-align: top;white-space:nowrap;">-r           <td>(recurse into sub-directories)
 	 * <tr><td style="vertical-align: top;white-space:nowrap;">-a           <td>(file of all types will be copy, default false)"
 	 * <tr><td style="vertical-align: top;white-space:nowrap;">-nob         <td>(no file backups required)
-	 * <tr><td> &nbsp;&nbsp; <td> &nbsp;&nbsp;   
+	 * <tr><td> &nbsp;&nbsp; <td> &nbsp;&nbsp;
 	 * <tr><td><td>*  - required parameter.
 	 * <tr><td><td>** - only required if -nob not provided.
 	 * </table></big></code></ul>
-	 * @return exitcode<br> 
+	 * @return exitcode<br>
 	 * -1 - user cancelled the request.<br>
 	 * -2 - an error occurred during processing<br>
 	 *  0 - +N The number of files that were modified.
 	 */
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
 		LibraryUpdate updater = null;
 		int exitcode = 0;
 		try{
@@ -744,7 +744,7 @@ public class LibraryUpdate {
 			if(!updater.isQuiet())  JOptionPane.showMessageDialog(null, x.getMessage()+"\n"+ updater.help_string, updater.prompt, JOptionPane.ERROR_MESSAGE);
 			exitcode = -2;
 		}
-		try{ 
+		try{
 			String mainclass = org.safs.tools.MainClass.deduceMainClass();
 			if( LibraryUpdate.class.getName().equals(mainclass) ||
 			    mainclass.endsWith("safsupdate.jar")	)
