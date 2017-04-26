6960f02ab7858f255ed7bca65721e39342594170
sbjlwa <lei.wang@sas.com>
2017-04-26 17:13:18 +0800

    Update script to download source code.

--- src/org/safs/selenium/install/extra/updateRuntime.bat
+++ src/org/safs/selenium/install/extra/updateRuntime.bat
@@ -11,14 +11,22 @@ REM         updateRuntime.bat ALL
 SETLOCAL
  
 REM External update resource
-REM SET SE_LIB_UPDATE=https://github.com/SAFSDEV/UpdateSite/releases/download/seleniumplus/SEPLUS.LIB.UPDATE.ZIP
-REM SET SE_PLUGIN_UPDATE=https://github.com/SAFSDEV/UpdateSite/releases/download/seleniumplus/SEPLUS.PLUGIN.UPDATE.ZIP
-REM SET SAFS_LIB_UPDATE=https://github.com/SAFSDEV/UpdateSite/releases/download/safs/SAFS.LIB.UPDATE.ZIP
+SET GITHUB_UPDATE=https://github.com/SAFSDEV/UpdateSite/releases/download
+REM SET SE_LIB_UPDATE=%GITHUB_UPDATE%/seleniumplus/SEPLUS.LIB.UPDATE.ZIP
+REM SET SE_PLUGIN_UPDATE=%GITHUB_UPDATE%/seleniumplus/SEPLUS.PLUGIN.UPDATE.ZIP
+REM SET SE_SOURCE_UPDATE=%GITHUB_UPDATE%/seleniumplus/source_all.zip
+REM SET SAFS_LIB_UPDATE=%GITHUB_UPDATE%/safs/SAFS.LIB.UPDATE.ZIP
+REM SET SE_SOURCE_UPDATE=%GITHUB_UPDATE%/safs/source_all.zip
 
 REM Internal update resource
-SET SE_LIB_UPDATE=http://safsbuild.na.sas.com:81/jenkins/job/SeleniumPlus/ws/updatesite/lib/latest/SEPLUS.LIB.UPDATE.ZIP
-SET SE_PLUGIN_UPDATE=http://safsbuild.na.sas.com:81/jenkins/job/SeleniumPlus/ws/updatesite/plugin/latest/SEPLUS.PLUGIN.UPDATE.ZIP
-SET SAFS_LIB_UPDATE=http://safsbuild.na.sas.com:81/jenkins/job/SAFS/ws/updatesite/lib/latest/SAFS.LIB.UPDATE.ZIP
+SET INTERNAL_SE_UPDATE=http://safsbuild.na.sas.com:81/jenkins/job/SeleniumPlus/ws/updatesite
+SET SE_LIB_UPDATE=%INTERNAL_SE_UPDATE%/lib/latest/SEPLUS.LIB.UPDATE.ZIP
+SET SE_PLUGIN_UPDATE=%INTERNAL_SE_UPDATE%/plugin/latest/SEPLUS.PLUGIN.UPDATE.ZIP
+SET SE_SOURCE_UPDATE=%INTERNAL_SE_UPDATE%/source/latest/source_all.zip
+
+SET INTERNAL_SAFS_UPDATE=http://safsbuild.na.sas.com:81/jenkins/job/SAFS/ws/updatesite
+SET SAFS_LIB_UPDATE=%INTERNAL_SAFS_UPDATE%/lib/latest/SAFS.LIB.UPDATE.ZIP
+SET SAFS_SOURCE_UPDATE=%INTERNAL_SAFS_UPDATE%/source/latest/source_all.zip
 
 REM GET PRODUCT NAME FROM INPUT PARAMETER, IT COULD BE ALL|SE|SAFS
 REM The default value is ALL
@@ -49,6 +57,7 @@ IF NOT EXIST %BACKUP_LIBDIR% MKDIR %BACKUP_LIBDIR%
 copy %SELENIUM_PLUS%\libs\safsupdate.jar %BACKUP_LIBDIR%\safsupdate.jar
 %JAVA_EXE% -jar %BACKUP_LIBDIR%\safsupdate.jar -r -a -q -prompt:"SeleniumPlus Libs Update" -s:"%SE_LIB_UPDATE%" -t:"%SELENIUM_PLUS%" -b:"%SELENIUM_PLUS%\update_bak"
 %JAVA_EXE% -jar %BACKUP_LIBDIR%\safsupdate.jar -r -a -q -prompt:"SeleniumPlus Plugin Update" -s:"%SE_PLUGIN_UPDATE%" -t:"%SELENIUM_PLUS%\eclipse\plugins" -b:"%SELENIUM_PLUS%\eclipse\plugins\update_bak"
+%JAVA_EXE% -jar %BACKUP_LIBDIR%\safsupdate.jar -r -a -q -prompt:"SeleniumPlus Source Update" -s:"%SE_SOURCE_UPDATE%" -t:"%SELENIUM_PLUS%" -b:"%SELENIUM_PLUS%\update_bak"
 IF NOT [%PRODUCT%]==[ALL] GOTO SHOW_INFO
 
 :UPDATE_SAFS
@@ -58,6 +67,7 @@ SET JAVA_EXE=%SAFSDIR%\jre\bin\java
 IF NOT EXIST %BACKUP_LIBDIR% MKDIR %BACKUP_LIBDIR%
 copy %SAFSDIR%\lib\safsupdate.jar %BACKUP_LIBDIR%\safsupdate.jar
 %JAVA_EXE% -jar %BACKUP_LIBDIR%\safsupdate.jar -r -a -q -prompt:"SAFS Libs Update" -s:"%SAFS_LIB_UPDATE%" -t:"%SAFSDIR%" -b:"%SAFSDIR%\update_bak"
+%JAVA_EXE% -jar %BACKUP_LIBDIR%\safsupdate.jar -r -a -q -prompt:"SAFS Source Update" -s:"%SAFS_SOURCE_UPDATE%" -t:"%SAFSDIR%" -b:"%SAFSDIR%\update_bak"
 IF NOT [%PRODUCT%]==[ALL] GOTO SHOW_INFO
 
 GOTO END
