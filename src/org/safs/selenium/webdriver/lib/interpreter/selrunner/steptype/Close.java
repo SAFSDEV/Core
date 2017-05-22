da7680d038f548091b7a6a8f8ea7ee560f74e09d
sbjlwa <lei.wang@sas.com>
2017-05-22 14:45:28 +0800

    Added a method SearchObject.isValidBrowserID(), and we use it to check the ID when starting/stopping browser.

--- src/org/safs/selenium/webdriver/lib/interpreter/selrunner/steptype/Close.java
+++ src/org/safs/selenium/webdriver/lib/interpreter/selrunner/steptype/Close.java
@@ -1,7 +1,10 @@
-/** 
+/**
  * Copyright (C) SAS Institute, All rights reserved.
  * General Public License: http://www.opensource.org/licenses/gpl-license.php
  **/
+/**
+ * MAY 22, 2017    (SBJLWA)  Modified run(): call WDLibrary.isValidBrowserID() to check the validity of browser ID.
+ */
 package org.safs.selenium.webdriver.lib.interpreter.selrunner.steptype;
 
 import org.safs.selenium.webdriver.lib.WDLibrary;
@@ -12,7 +15,7 @@ import com.sebuilder.interpreter.StepType;
 import com.sebuilder.interpreter.TestRun;
 
 /**
- * Supports an optional "browserid" parameter which can be used to reference the specific browser instance 
+ * Supports an optional "browserid" parameter which can be used to reference the specific browser instance
  * to close.  Generally, it is the same browserid that would be used in the Open command.
  * @author canagl
  * @see Open
@@ -20,14 +23,14 @@ import com.sebuilder.interpreter.TestRun;
 public class Close implements StepType, SRunnerType {
 
 	public static String BROWSERID_PARAM = "browserid";
-	
+
 	/* (non-Javadoc)
 	 * @see com.sebuilder.interpreter.StepType#run(com.sebuilder.interpreter.TestRun)
 	 */
 	@Override
 	public boolean run(TestRun ctx) {
 		String bid = ctx.string(BROWSERID_PARAM);
-		if(bid == null || bid.length()==0){
+		if(!WDLibrary.isValidBrowserID(bid)){
 			bid = WDLibrary.getIDForWebDriver(ctx.getDriver());
 		}
 		try{
