343d63766ba239e6a06b3f6b05fff1ba4ab7bea8
John W. Lewis <johnw.lewis@sas.com>
2017-06-05 15:10:28 -0400

    Move SELENIUM_PLUS_ENV. SELENIUM_PLUS_ENV is moved from com.sas.seleniumplus.projects.BaseProject to org.safs.seleniumplus.projects.BaseProject in SAFS Core.

---
+++ src/org/safs/seleniumplus/projects/BaseProject.java
@@ -0,0 +1,6 @@
+package org.safs.seleniumplus.projects;
+
+public class BaseProject {
+	/** "SELENIUM_PLUS" the system environment variable name holding the path where SeleniumPlus has been installed */
+	public static final String SELENIUM_PLUS_ENV = "SELENIUM_PLUS";
+}
