63dcffe7c32860556a312cde3c7c379cda7156ac
John W. Lewis <johnw.lewis@sas.com>
2017-06-06 11:26:43 -0400

    Remove SELENIUM_PLUS_ENV and add SELENIUM_PLUS.

--- src/org/safs/seleniumplus/projects/BaseProject.java
+++ src/org/safs/seleniumplus/projects/BaseProject.java
@@ -1,6 +1,6 @@
 package org.safs.seleniumplus.projects;
 
 public class BaseProject {
-	/** "SELENIUM_PLUS" the system environment variable name holding the path where SeleniumPlus has been installed */
-	public static final String SELENIUM_PLUS_ENV = "SELENIUM_PLUS";
+	/** holds path to SeleniumPlus install directory -- once validated. */
+	public static String SELENIUM_PLUS;
 }
